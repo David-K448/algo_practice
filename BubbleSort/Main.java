d956df67ba3453a2d4380220522895411ac32215
David Killian <killianda36@gmail.com>
2022-05-20 00:40:32 -0400

    newcommit

--- BubbleSort/Main.java
+++ BubbleSort/Main.java
@@ -1,9 +1,100 @@
-import java.util.*;
 import javax.swing.*;
+import java.util.Random;
 import java.awt.*;
+import java.util.*;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.awt.event.*;
+import java.util.concurrent.ThreadLocalRandom;
+import java.awt.Graphics;
 
-public class Main {
-  public static void main(String[] args) {
+public class Main{
+  static JFrame frame;
+  static JPanel panel1; 
+  static int[] randArray = new int[200];
+  static Random r = new Random();
+  static int min = 5;
+  static int max = 700;
 
+  public Main() {
+    prepGui();
+    buildGui();
   }
+
+  private static void prepGui() {
+    for(int i = 0; i < randArray.length; i++){
+      randArray[i]  = r.nextInt(max-min) + min;
+    }
+    
+    frame = new JFrame();
+    panel1 = new JPanel();//"new GridBagLayout()"
+    frame.add(panel1);
+
+    GridBagConstraints c = new GridBagConstraints();
+    //c.weightx = 1;
+    //c.weighty = 1;
+    //c.fill = GridBagConstraints.HORIZONTAL;
+
+    JLabel title = new JLabel("Algorithm Visualizer");
+    title.setFont(new Font("Verdana", Font.PLAIN, 30));
+    
+
+    JLabel subtitle = new JLabel("Select an algorithm from below,");
+    subtitle.setSize(425, 200);
+    subtitle.setFont(new Font("Verdana", Font.PLAIN, 18));
+
+    JLabel subtitle2 = new JLabel("and create a new array to begin.");
+    subtitle2.setSize(425, 200);
+    subtitle2.setFont(new Font("Verdana", Font.PLAIN, 18));
+    
+    panel1.add(title);
+    panel1.add(subtitle);
+    panel1.add(subtitle2);
+
+    JButton bubble = new JButton("Bubble Sort");
+    panel1.add(bubble);
+
+    JButton newArray = new JButton("New Array");
+    panel1.add(newArray);
+
+
+
+    ////////////////////
+    //Action Listeners//
+    ////////////////////
+
+    newArray.addActionListener(new ActionListener() {
+      @Override
+      public void actionPerformed(ActionEvent evt) {
+        for(int i = 0; i < randArray.length; i++){
+          randArray[i]  = r.nextInt(max-min) + min;
+        }
+        for(int n : randArray) {
+          System.out.print(n+"\n");
+        }
+      }
+    });
+
+    bubble.addActionListener(new ActionListener() {
+      @Override
+      public void actionPerformed(ActionEvent evt) {
+        
+
+        
+      }
+    });
+  }
+
+
+
+  private static void buildGui() {
+    frame.setSize(500, 750);
+    frame.setVisible(true);
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE );
+    frame.setLocationRelativeTo(null);
+  }
+
+  public static void main(String[] args) {
+   Main main = new Main();
+  }
+  
 }
